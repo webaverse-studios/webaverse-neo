64db16055fe748ec7d3627e748a5103842d8f2dd
Jacob LeCoq <lecoqjacob@gmail.com>
2023-03-15 15:17:38 -0500

    Update asset types

--- packages/utils/types/assets/index.js
+++ packages/utils/types/assets/index.js
@@ -1,22 +1,22 @@
-/* eslint-disable max-len */
-
-import { AnimationClip, Group, Camera } from 'three'
+/**
+ * @typedef {object} GLTFAsset
+ * @property {string} [copyright] Copyright
+ * @property {string} [generator] Generator
+ * @property {string} [version] Version
+ * @property {string} [minVersion] Minimum Version
+ * @property {any} [extensions] Extensions
+ * @property {any} [extras] Extras
+ */
 
 /**
- * @typedef {{
- * animations: AnimationClip[],
- * scene: Group,
- * scenes: Group[],
- * cameras: Camera[],
- * asset: {
- *  copyright?: string | undefined;
- *  generator?: string | undefined;
- *  version?: string | undefined;
- *  minVersion?: string | undefined;
- *  extensions?: any;
- *  extras?: any;
- * }
- * parser: GLTFParser
- * userData: any;
- * }} GLTF
+ * @typedef {object} GLTF
+ * @property {any} userData User Data
+ * @property {GLTFAsset} asset Asset
+ * @property {import('three').Group} scene Scene
+ * @property {import('three').Group[]} scenes Scenes
+ * @property {import('three').Camera[]} cameras Cameras
+ * @property {import('three').AnimationClip[]} animations Animations
+ * @property {import('three/examples/jsm/loaders/GLTFLoader').GLTFParser} parser Parser
  */
+
+export default {}
