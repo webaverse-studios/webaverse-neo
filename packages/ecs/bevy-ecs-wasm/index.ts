c0e26c02b0c45b7efc0c917c6236b1dcb40aa38e
Jacob LeCoq <lecoqjacob@gmail.com>
2023-03-18 20:25:03 -0500

    :/

--- packages/ecs/bevy-ecs-wasm/index.ts
+++
@@ -1,7 +0,0 @@
-import { World } from './pkg'
-
-let world = new World()
-console.log(world.toString())
-console.log(world.getComponents(), typeof world.getComponents())
-console.log(world.getResources(), typeof world.getResources())
-console.log(world.getResources(), typeof world.getResources())
